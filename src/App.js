ded8e34da33acbd9477657273f7bbd583d1f9e32
pamny001 <pamny001@mymail.unisa.edu.au>
2025-10-23 23:35:55 +1030

    Created controlButtons component
    Transferred the code for On and Hush buttons to it's own react component.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/w
 import { registerSoundfonts } from '@strudel/soundfonts';
 import { stranger_tune } from './tunes';
 import console_monkey_patch, { getD3Data } from './console-monkey-patch';
+import ControlButtons from './components/controlButtons';
 
 let globalEditor = null;
 
@@ -134,18 +135,7 @@ return (
                         <div id="output" />
                     </div>
                     <div className="col-md-4">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
-                            <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                p1: ON
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
-                            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                p1: HUSH
-                            </label>
-                        </div>
+                        <ControlButtons />
                     </div>
                 </div>
             </div>
