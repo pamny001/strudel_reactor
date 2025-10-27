e0e91aa508ba20a14e718e1cb2ecc0861ffd4d1f
pamny001 <pamny001@mymail.unisa.edu.au>
2025-10-27 19:39:52 +1030

    Added titles and margin

--- src/components/controlButtons.jsx
+++ src/components/controlButtons.jsx
@@ -9,18 +9,19 @@ function ControlButtons({ controls, onChangeMode, onChangeBpm }) {
     <>
       <p className="d-inline-flex gap-1 collapse-buttons">
         <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#speedCollapse" aria-expanded="false" aria-controls="speedCollapse">Speed</button>
-        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#mainArpCollapse" aria-expanded="false" aria-controls="mainArpCollapse">Main Arp</button>
         <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#bassLineCollapse" aria-expanded="false" aria-controls="bassLineCollapse">Bass Line</button>
+        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#mainArpCollapse" aria-expanded="false" aria-controls="mainArpCollapse">Main Arp</button>
         <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#drums1Collapse" aria-expanded="false" aria-controls="drums1Collapse">Drums 1</button>
         <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#drums2Collapse" aria-expanded="false" aria-controls="drums2Collapse">Drums 2</button>
       </p>
 
-      <div className="row mt-3">
+      <div className="row">
         {/* Speed Section */}
         <div className="col-12">
           <div className="collapse" id="speedCollapse">
             <div className="card card-body">
 
+            <h5 className="mb-3 text-center">Speed</h5>
               <div className="input-group">
                 <div className="input-group-text" id="btnGroupAddon">BPM</div>
                 <input type="number" className="form-control" placeholder="Enter BPM" aria-label="BPM Input" aria-describedby="btnGroupAddon" 
@@ -33,26 +34,12 @@ function ControlButtons({ controls, onChangeMode, onChangeBpm }) {
           </div>
         </div>
 
-        {/* Main Arp */}
-        <div className="col-12 mt-3">
-          <div className="collapse" id="mainArpCollapse">
-            <div className="card card-body">
-
-                <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckMode" checked={controls.mainArp === "on"} 
-                    onChange={(e) => onChangeMode?.("mainArp", e.target.checked ? "on" : "off")} />
-                    <label className="form-check-label" htmlFor="flexSwitchCheckMode">Main Arp</label>
-                </div>
-
-            </div>
-          </div>
-        </div>
-
         { /* Bass Line */ }
-        <div className="col-12 mt">
+        <div className="col-12 mt-3">
           <div className="collapse" id="bassLineCollapse">
             <div className="card card-body">
 
+                <h5 className="mb-3 text-center">Bass Line Controls</h5>
                 <div className="form-check form-switch">
                     <input className="form-check-input" type="checkbox" id="flexSwitchCheckMode" checked={controls.bassLine === "on"} 
                     onChange={(e) => onChangeMode?.("bassLine", e.target.checked ? "on" : "off")} />
@@ -63,11 +50,28 @@ function ControlButtons({ controls, onChangeMode, onChangeBpm }) {
           </div>
         </div>
 
+                {/* Main Arp */}
+        <div className="col-12 mt-3">
+          <div className="collapse" id="mainArpCollapse">
+            <div className="card card-body">
+
+                <h5 className="mb-3 text-center">Main Arp Controls</h5>
+                <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckMode" checked={controls.mainArp === "on"} 
+                    onChange={(e) => onChangeMode?.("mainArp", e.target.checked ? "on" : "off")} />
+                    <label className="form-check-label" htmlFor="flexSwitchCheckMode">Main Arp</label>
+                </div>
+
+            </div>
+          </div>
+        </div>
+
         { /* Drums1 */}
-        <div className="col-12 mt">
+        <div className="col-12 mt-3">
           <div className="collapse" id="drums1Collapse">
             <div className="card card-body">
 
+                <h5 className="mb-3 text-center">Drums 1 Controls</h5>
                 <div className="form-check form-switch">
                     <input className="form-check-input" type="checkbox" id="flexSwitchCheckMode" checked={controls.drums1 === "on"} 
                     onChange={(e) => onChangeMode?.("drums1", e.target.checked ? "on" : "off")} />
@@ -79,10 +83,11 @@ function ControlButtons({ controls, onChangeMode, onChangeBpm }) {
         </div>
 
         { /* Drums2 */}
-        <div className="col-12 mt">
+        <div className="col-12 mt-3">
           <div className="collapse" id="drums2Collapse">
             <div className="card card-body">
 
+                <h5 className="mb-3 text-center">Drums 2 Controls</h5>
                 <div className="form-check form-switch">
                     <input className="form-check-input" type="checkbox" id="flexSwitchCheckMode" checked={controls.drums2 === "on"} 
                     onChange={(e) => onChangeMode?.("drums2", e.target.checked ? "on" : "off")} />
