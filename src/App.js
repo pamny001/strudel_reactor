28be7a2100fefde2e02935642c81647e11fc004b
pamny001 <pamny001@mymail.unisa.edu.au>
2025-11-04 16:12:01 +1030

    Removed old javascript code

--- src/App.js
+++ src/App.js
@@ -21,51 +21,6 @@ const handleD3Data = (event) => {
     console.log(event.detail);
 };
 
-/* export function SetupButtons() {
-
-    //document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-    //document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-    document.getElementById('process').addEventListener('click', () => {
-        Proc()
-    }
-    )
-    document.getElementById('process_play').addEventListener('click', () => {
-        if (globalEditor != null) {
-            Proc()
-            globalEditor.evaluate()
-        }
-    }
-    )
-} */
-
-
-
-/* export function ProcAndPlay() {
-    if (globalEditor != null && globalEditor.repl.state.started == true) {
-        console.log(globalEditor)
-        Proc()
-        globalEditor.evaluate();
-    }
-} */
-
-/* export function Proc() {
-
-    let proc_text = document.getElementById('proc').value
-    let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
-    ProcessText(proc_text);
-    globalEditor.setCode(proc_text_replaced)
-} */
-
-/* export function ProcessText(match, ...args) {
-
-    let replace = ""
-    if (document.getElementById('flexRadioDefault2').checked) {
-        replace = "_"
-    }
-
-    return replace
-} */
-
 //Process song and check for control settings. 
 function processSong(text, controls) {
     //Song code before adding controls changes
