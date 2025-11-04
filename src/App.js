539047fc39d7c2c9abbe7b6d4ebecd43aca59e48
pamny001 <pamny001@mymail.unisa.edu.au>
2025-11-04 16:10:32 +1030

    Moving saveload in playbutton

--- src/App.js
+++ src/App.js
@@ -225,27 +225,27 @@ return (
                     <div className="col-md-4">
                         {/*Play/Stop/Preprocess/Proc&Play*/}
                         <PlayBackButtons onPlay={playButton} onStop={stopButton}>
-                            <SaveLoadButtons
-                                appState={{
-                                    songText,
-                                    controls,
-                                    defaultSongText: stranger_tune,
-                                    defaultControls: {
-                                    bpm: 140,
-                                    bassLine: "on",
-                                    bassLpf: 700,
-                                    mainArp: "on",
-                                    mainArpLpf: 300,
-                                    mainArpRoom: 0.6,
-                                    mainArpLpenv: 3.3,
-                                    drums1: "on",
-                                    drums2: "on",
-                                    },
-                                }}
-                                setAppStateLoad={setAppStateLoad}
-                                setAppStateReset={setAppStateReset}
-                            />
-                        </PlayBackButtons>
+                        <SaveLoadButtons
+                        appState={{
+                            songText,
+                            controls,
+                            defaultSongText: stranger_tune,
+                            defaultControls: {
+                            bpm: 140,
+                            bassLine: "on",
+                            bassLpf: 700,
+                            mainArp: "on",
+                            mainArpLpf: 300,
+                            mainArpRoom: 0.6,
+                            mainArpLpenv: 3.3,
+                            drums1: "on",
+                            drums2: "on",
+                            },
+                        }}
+                        setAppStateLoad={setAppStateLoad}
+                        setAppStateReset={setAppStateReset}
+                        />
+                        </PlayBackButtons> 
                     </div>
                 </div>
                 <div className="row">
